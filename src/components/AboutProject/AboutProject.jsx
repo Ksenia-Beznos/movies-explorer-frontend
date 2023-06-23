2da097b0fcf8185016e6ab8c9291caa420a0326e
Kseniia <ksenia.beznos@yandex.ru>
2023-06-24 00:30:13 +0300

    320px done

--- src/components/AboutProject/AboutProject.jsx
+++ src/components/AboutProject/AboutProject.jsx
@@ -5,17 +5,23 @@ function AboutProject() {
 		<>
 			<div className="project">
 				<h2 className="project__header">О проекте</h2>
-				<div className="project__descriptions">
-					<p className="project__description project__title">Дипломный проект включал 5 этапов</p>
-					<p className="project__description project__title">На выполнение диплома ушло 5 недель</p>
-					<p className="project__description project__subtitle">
-						Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные
-						доработки.
-					</p>
-					<p className="project__description project__subtitle">
-						У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы
-						успешно защититься.
-					</p>
+				<div className="project__chart">
+					<div className="project__stages">
+						<p className="project__description project__title">Дипломный проект включал 5 этапов</p>
+						<p className="project__description project__subtitle">
+							Составление плана, работу над бэкендом, вёрстку, добавление функциональности и
+							финальные доработки.
+						</p>
+					</div>
+					<div className="project__timing">
+						<p className="project__description project__title">
+							На выполнение диплома ушло 5 недель
+						</p>
+						<p className="project__description project__subtitle">
+							У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы
+							успешно защититься.
+						</p>
+					</div>
 				</div>
 				<div className="timing">
 					<p className="timing__week timing__week_color_green">1 неделя</p>
