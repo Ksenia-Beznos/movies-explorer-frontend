225ab937655e48755c01dc4c22243fbd50d8b877
Kseniia <ksenia.beznos@yandex.ru>
2023-07-01 00:44:54 +0300

    fix step-3

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -3,17 +3,47 @@ import './SearchForm.css';
 import find from '../../images/find.svg';
 import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
 import line from '../../images/line.svg';
+import { useForm } from 'react-hook-form';
 
 function SearchForm() {
+	const {
+		register,
+		formState: { errors },
+		handleSubmit,
+		reset,
+	} = useForm({
+		mode: 'onChange',
+	});
+
+	const onSubmit = (data) => {
+		console.log({ data });
+		reset();
+	};
+
 	return (
-		<form className="search-form">
+		<form className="search-form" onSubmit={handleSubmit(onSubmit)}>
 			<input
 				id="name-input"
 				className="search-form__input"
 				type="text"
 				name="movieInput"
 				placeholder="Фильм"
+				autoComplete="off"
+				{...register('movieInput', {
+					required: 'Введите название фильма',
+					pattern: {
+						value: /^[^\s]/,
+						message: 'Название фильма не может начинаться с пробела',
+					},
+				})}
 			/>
+
+			{errors?.movieInput && (
+				<span className="search-form__input-error">
+					{errors?.movieInput?.message || 'Что-то пошло не так'}
+				</span>
+			)}
+
 			<button className="search-form__button" type="submit">
 				<img src={find} alt="Кнопка в виде синей стрелки" />
 			</button>
